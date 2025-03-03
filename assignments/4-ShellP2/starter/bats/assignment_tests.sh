fdd585881697f867ed0723b904632f4da0b61b1b
Alyckh <ahodgeboorah@gmail.com>
2025-03-02 19:56:55 -0600

    Update assignment_tests.sh

--- assignments/4-ShellP2/starter/bats/assignment_tests.sh
+++ assignments/4-ShellP2/starter/bats/assignment_tests.sh
@@ -10,20 +10,6 @@
 # All tests in this file must pass - it is used as part of grading!
 ########################################################################################
 
-# Create a symbolic link to dsh if it doesn't exist
-setup() {
-    # Get the absolute path of the "starter" directory (one level up from "bats")
-    starter_dir="$(dirname "$(pwd)")/starter"
-
-    # Check if the symbolic link exists in the bats folder
-    if [ ! -L "./dsh" ]; then
-        ln -s "$starter_dir/dsh" ./dsh
-        echo "Symlink created for dsh"
-    else
-        echo "Symlink for dsh already exists"
-    fi
-}
-
 @test "Change directory" {
     current=$(pwd)
 
